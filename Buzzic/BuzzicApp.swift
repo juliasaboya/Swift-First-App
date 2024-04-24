6231dbeaccdbc9c8bd33a58bf1efc0652fa6f991
julia <juliasaboyaengenharia@gmail.com>
2024-04-24 14:31:29 -0300

    Initial Commit

---
+++ Buzzic/BuzzicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzicApp.swift
+//  Buzzic
+//
+//  Created by User on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BuzzicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
